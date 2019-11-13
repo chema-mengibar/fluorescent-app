eb5bab5e5d88dd2c8ff78404620070dc7449cc83
Unknown <chemamengibar@gmail.com>
2019-11-13 07:27:11 +0100

    ADD: parse tasks defined

---
+++ server/parser.js
@@ -0,0 +1,22 @@
+/*
+  Variables:
+
+  component name:
+    - how is the rule to name components
+    - node-label <-> component folder name
+
+  atomic-type folder name :
+    - if component-folder exist in folder
+
+  list of children 
+    - if exist import line with child-component-name in code
+
+  
+  Neeeds: 
+
+  - get string conversion from node-label to component-name/folder
+  - read directories
+  - read content file
+  - parse import lines
+
+*/
